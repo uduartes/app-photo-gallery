974439c7e34726b4af2b7df9ad8b93e44953ed57
Flavio Cesar Fudino Gonzales <112134642+FlavioFudino@users.noreply.github.com>
2024-03-21 13:23:15 -0500

    saving data to file System

--- src/app/services/photo.service.ts
+++ src/app/services/photo.service.ts
@@ -29,9 +29,42 @@ export class PhotoService {
       quality: 100,
     });
 
-    this.photos.unshift({
-      filepath: '',
-      webviewPath: capturedPhoto.webPath,
-    });
+    const savedImageFile = await this.savePicture(capturedPhoto);
+
+    this.photos.unshift(savedImageFile);
   }
+
+  private async savePicture(photo: Photo) {
+    const base64Data = await this.readAsBase64(photo);
+
+    const fileName = new Date().getTime() + '.jpeg';
+    const savedFile = await Filesystem.writeFile({
+      path: fileName,
+      data: base64Data,
+      directory: Directory.Data,
+    });
+
+    return {
+      filepath: fileName,
+      webviewPath: photo.webPath,
+    };
+  }
+
+  private async readAsBase64(photo: Photo) {
+    // Fetch the photo, read as a blob, then convert to base64 format
+    const response = await fetch(photo.webPath!);
+    const blob = await response.blob();
+
+    return (await this.convertBlobToBase64(blob)) as string;
+  }
+
+  private convertBlobToBase64 = (blob: Blob) =>
+    new Promise((resolve, reject) => {
+      const reader = new FileReader();
+      reader.onerror = reject;
+      reader.onload = () => {
+        resolve(reader.result);
+      };
+      reader.readAsDataURL(blob);
+    });
 }
