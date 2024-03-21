a9ba965a74d8225555b62e76951fa8fb6747669b
Flavio Cesar Fudino Gonzales <112134642+FlavioFudino@users.noreply.github.com>
2024-03-21 12:44:04 -0500

    primer commit

---
+++ src/app/services/photo.service.ts
@@ -0,0 +1,37 @@
+import { Injectable } from '@angular/core';
+import {
+  Camera,
+  CameraResultType,
+  CameraSource,
+  Photo,
+} from '@capacitor/camera';
+import { Capacitor } from '@capacitor/core';
+import { Filesystem, Directory } from '@capacitor/filesystem';
+import { Preferences } from '@capacitor/preferences';
+
+export interface UserPhoto {
+  filepath: string;
+  webviewPath?: string;
+}
+
+@Injectable({
+  providedIn: 'root',
+})
+export class PhotoService {
+  public photos: UserPhoto[] = [];
+
+  constructor() {}
+
+  public async addNewToGallery() {
+    const capturedPhoto = await Camera.getPhoto({
+      resultType: CameraResultType.Uri,
+      source: CameraSource.Camera,
+      quality: 100,
+    });
+
+    this.photos.unshift({
+      filepath: '',
+      webviewPath: capturedPhoto.webPath,
+    });
+  }
+}
